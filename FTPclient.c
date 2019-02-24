ee6a771ae0eba657537484106fc2b47eeac674f9
msn307 <22077758+Munachiso-Nwadike@users.noreply.github.com>
2019-02-24 18:55:13 +0400

    skeleton for the 11 commands

--- FTPclient.c
+++ FTPclient.c
@@ -10,13 +10,14 @@
     
 int main(int argc, char const *argv[]) 
 { 
-    struct sockaddr_in address; 
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
+    struct sockaddr_in cl_address; 
+    struct sockaddr_in server_addr;
+    int soc = 0; 
     char *hello = "Hello from client"; 
 
     //MY edit
-    char outmsg[100] = {0};
+    char outmsg[5] = {0};
+    char outmsg1[10] = {0};
     int port; char ip[15] = {0};
     port = atoi(argv[2]);
     sscanf(argv[1], "%s", ip);
@@ -24,46 +25,162 @@ int main(int argc, char const *argv[])
     fflush(stdin);
 
     //
+    int valread;
+
     char buffer[1024] = {0}; 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
+    if ((soc = socket(AF_INET, SOCK_STREAM, 0)) < 0){ 
         printf("\n Socket creation error \n"); 
         return -1; 
     } 
    
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
+    memset(&server_addr, '0', sizeof(server_addr)); 
    
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(port); 
+    server_addr.sin_family = AF_INET; 
+    server_addr.sin_port = htons(port); 
        
     // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
-    { 
+    if(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr)<=0){ 
         printf("\nInvalid address/ Address not supported \n"); 
         return -1; 
     } 
    
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
+    if (connect(soc, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){ 
         printf("\nConnection Failed \n"); 
         return -1; 
     } 
 
      
-	send(sock , hello, strlen(hello) , 0 ); 
-    valread = read( sock , buffer, 1024); 
-    printf("%s\n\n",buffer ); 
+    valread = read( soc , buffer, 1024); 
+    printf("%s\n\n", buffer ); 
     fflush(stdin);
 
     while(1){
     	printf("ftp> ");
-    	memset(outmsg, 0, sizeof(outmsg));
+    	fflush(stdin);
+    	memset(outmsg, 0, sizeof(outmsg)); memset(outmsg1, 0, sizeof(outmsg1));
     	scanf("%s", outmsg);
-	    send(sock , outmsg, strlen(outmsg) , 0 ); 
-		memset(buffer, 0, sizeof(buffer));
-	    valread = read( sock , buffer, 1024); 
-	    printf("%s\n\n",buffer ); 
-	    fflush(stdin);
+    	// authenticate User
+    	if (strncmp(outmsg, "USER", 4)==0){ 
+    		scanf("%s", outmsg);
+    		printf("ftp> Thanks, now please include password\n\n");
+    		printf("ftp> ");
+    		scanf("%s", outmsg1);
+    		if (strncmp(outmsg1, "PASS", 4)==0){ 
+    			// get the passowrd and send both user and pass to server
+				scanf("%s", outmsg1);
+				send(soc, outmsg, strlen(outmsg) , 0 );
+  		  		send(soc, outmsg1, strlen(outmsg1) , 0 );
+				
+				// read the server response out to screen
+				memset(buffer, 0, sizeof(buffer));
+			    valread = read( soc , buffer, 1024); 
+			    printf("About to print buffer content\n"); 
+			    fflush(stdin);
+			    printf("%s\n\n",buffer ); 
+			    fflush(stdin);
+
+			    // given user name and passowrd, should be able to run all other commands
+				while(1){
+					printf("ftp> ");
+			    	fflush(stdin);
+			    	memset(outmsg, 0, sizeof(outmsg)); memset(outmsg1, 0, sizeof(outmsg1));
+			    	scanf("%s", outmsg);
+					// handle the put command to put a file to remote from local
+					if (strncmp(outmsg, "PUT", 3)==0){ 
+			    		scanf("%s", outmsg1);
+			    		send(soc, outmsg1, strlen(outmsg1) , 0 );
+						memset(buffer, 0, sizeof(buffer));
+					    valread = read( soc , buffer, 1024); 
+					    printf("%s\n\n",buffer ); 
+					    fflush(stdin);
+				    // handle the get command to get a file from remote
+					}else if (strncmp(outmsg, "GET", 3)==0){ 
+			    		scanf("%s", outmsg1);
+			    		send(soc, outmsg1, strlen(outmsg1) , 0 );
+						memset(buffer, 0, sizeof(buffer));
+					    valread = read( soc , buffer, 1024); 
+					    printf("%s\n\n",buffer ); 
+					    fflush(stdin);
+					// handle the remote cd command
+					}else if (strncmp(outmsg, "CD", 2)==0){ 
+			    		scanf("%s", outmsg1);
+			    		send(soc, outmsg1, strlen(outmsg1) , 0 );
+						memset(buffer, 0, sizeof(buffer));
+					    valread = read( soc , buffer, 1024); 
+					    printf("%s\n\n",buffer ); 
+					    fflush(stdin);
+					// handle the remote ls command
+					}else if (strncmp(outmsg, "LS", 2)==0){ 
+			    		scanf("%s", outmsg1);
+			    		send(soc, outmsg1, strlen(outmsg1) , 0 );
+						memset(buffer, 0, sizeof(buffer));
+					    valread = read( soc , buffer, 1024); 
+					    printf("%s\n\n",buffer ); 
+					    fflush(stdin);
+				    // handle the remote pwd command to display current directory
+					}else if (strncmp(outmsg, "PWD", 3)==0){ 
+			    		scanf("%s", outmsg1);
+			    		send(soc , outmsg, strlen(outmsg) , 0 );
+						memset(buffer, 0, sizeof(buffer));
+					    valread = read( soc , buffer, 1024); 
+					    printf("%s\n\n",buffer ); 
+					    fflush(stdin);
+				    // handle the local ls command 
+					}else if (strncmp(outmsg, "!LS", 3)==0){ 
+			    		scanf("%s", outmsg1);
+			    		send(soc, outmsg1, strlen(outmsg1) , 0 );
+						memset(buffer, 0, sizeof(buffer));
+					    valread = read( soc , buffer, 1024); 
+					    printf("%s\n\n",buffer ); 
+					    fflush(stdin);
+				    // handle the local pwd command to display current directory
+					}else if (strncmp(outmsg, "!PWD", 4)==0){ 
+			    		scanf("%s", outmsg1);
+			    		send(soc , outmsg, strlen(outmsg) , 0 );
+						memset(buffer, 0, sizeof(buffer));
+					    valread = read( soc , buffer, 1024); 
+					    printf("%s\n\n",buffer ); 
+					    fflush(stdin);
+					// handle the local cd command to change current directory
+					}else if (strncmp(outmsg, "!CD", 3)==0){ 
+			    		scanf("%s", outmsg1);
+			    		send(soc, outmsg1, strlen(outmsg1) , 0 );
+						memset(buffer, 0, sizeof(buffer));
+					    valread = read( soc , buffer, 1024); 
+					    printf("%s\n\n",buffer ); 
+					    fflush(stdin);
+					// quit the ftp process
+					}else if (strncmp(outmsg, "QUIT", 4)==0){ 
+			    		printf("%s\n\n", "Goodbye!" ); 
+					    fflush(stdin);
+			    		close(soc);
+			    		exit(1);
+			    	}
+				}
+			}
+
+		// if we couldnt autheticate user, only allow to quit or ask to authenticate first
+		}else if (strncmp(outmsg, "QUIT", 4)==0){ 
+    		printf("%s\n\n", "Goodbye!" ); 
+		    fflush(stdin);
+    		close(soc);
+    		exit(1);
+		
+		// ask to authenticate first if not done
+		}else{
+			printf("Please authenticate Username and Password\n"); 
+		    fflush(stdin);
+		}
 	}
+
+	// break into cases for USER
+	// PASS
+	// PUT
+	// GET
+	// CD
+	// LS PWD
+	// !CD !LS !PWD
+	// QUIT
+
 	return 0;
 } 
--- FTPserver.c
+++ FTPserver.c
@@ -11,156 +11,131 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros  
+ 
      
-#define TRUE   1  
-#define FALSE  0  
-#define PORT 8888  
-     
-int main(int argc , char *argv[])   
-{   
-    int opt = TRUE;   
-    int master_socket , addrlen , new_socket , client_socket[30] ,  
-          max_clients = 30 , activity, i , valread , sd;   
-    int max_sd;   
+int main(int argc , char *argv[]){   
+    int opt = 1;   
+    int mastsoc , addrlen , newsoc , clsocs[30], maxcls = 30;
+    int activity , valread , sd;   
+    int maxsd;   
+    int port = 8888;
     struct sockaddr_in address;   
          
     char buffer[1025];  //data buffer of 1K  
          
-    //set of socket descriptors  
-    fd_set readfds;   
-         
-    //a message  
+    //set of socket descriptors and in an array as well so we can loop through them
+
+    fd_set readfds;    
+    for (int i = 0; i < maxcls; i++){   
+        clsocs[i] = 0;   
+    }  
+
+    // welcome message
     char *message = "Munachiso's FTP Server 0.1 \r\n";   
      
-    //initialise all client_socket[] to 0 so not checked  
-    for (i = 0; i < max_clients; i++)   
-    {   
-        client_socket[i] = 0;   
-    }   
+      
          
-    //create a master socket  
-    if( (master_socket = socket(AF_INET , SOCK_STREAM , getprotobyname("TCP")->p_proto)) == 0)   
-    {   
+    //master socket  
+    if( (mastsoc = socket(AF_INET , SOCK_STREAM , getprotobyname("TCP")->p_proto)) == 0){   
         perror("socket failed");   
         exit(EXIT_FAILURE);   
     }   
      
-    //set master socket to allow multiple connections ,  
-    //this is just a good habit, it will work without this  
-    if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,  
-          sizeof(opt)) < 0 )   
-    {   
+    //master socket should allow multpile connections 
+    if( setsockopt(mastsoc, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,  
+          sizeof(opt)) < 0 ){   
         perror("setsockopt");   
         exit(EXIT_FAILURE);   
     }   
      
-    //type of socket created  
+    //setting address
     address.sin_family = AF_INET;   
     address.sin_addr.s_addr = INADDR_ANY;   
-    address.sin_port = htons( PORT );   
+    address.sin_port = htons(port);   
          
-    //bind the socket to localhost port 8888  
-    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)   
-    {   
+    //binding socket to localhost port 8888  
+    if (bind(mastsoc, (struct sockaddr *)&address, sizeof(address))<0){   
         perror("bind failed");   
         exit(EXIT_FAILURE);   
     }   
-    printf("Listener on port %d \n", PORT);   
+    printf("Listener on port %d \n", port);   
          
-    //try to specify maximum of 3 pending connections for the master socket  
-    if (listen(master_socket, 3) < 0)   
-    {   
+    //max of three pending connections for master socket
+    if (listen(mastsoc, 3) < 0){   
         perror("listen");   
         exit(EXIT_FAILURE);   
     }   
-         
-    //accept the incoming connection  
     addrlen = sizeof(address);   
-    puts("Waiting for connections ...");   
-         
-    while(TRUE)   
-    {   
+    //accept the incoming connections
+    puts("Waiting for connections ...");       
+    while(1){   
         //clear the socket set  
         FD_ZERO(&readfds);   
      
         //add master socket to set  
-        FD_SET(master_socket, &readfds);   
-        max_sd = master_socket;   
+        FD_SET(mastsoc, &readfds);   
+        maxsd = mastsoc;   
              
         //add child sockets to set  
-        for ( i = 0 ; i < max_clients ; i++)   
-        {   
+        for (int i = 0; i < maxcls; i++){   
             //socket descriptor  
-            sd = client_socket[i];   
+            sd = clsocs[i];   
                  
             //if valid socket descriptor then add to read list  
             if(sd > 0)   
                 FD_SET( sd , &readfds);   
                  
             //highest file descriptor number, need it for the select function  
-            if(sd > max_sd)   
-                max_sd = sd;   
+            if(sd > maxsd)   
+                maxsd = sd;   
         }   
      
-        //wait for an activity on one of the sockets , timeout is NULL ,  
-        //so wait indefinitely  
-        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);   
-       
-        if ((activity < 0) && (errno!=EINTR))   
-        {   
+        //wait indefinitely for an activity on one of the sockets (timeout==NULL here) 
+        activity = select( maxsd + 1 , &readfds , NULL , NULL , NULL);   
+        if ((activity < 0) && (errno!=EINTR)){   
             printf("select error");   
         }   
              
-        //If something happened on the master socket ,  
-        //then its an incoming connection  
-        if (FD_ISSET(master_socket, &readfds))   
-        {   
-            if ((new_socket = accept(master_socket,  
-                    (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)   
-            {   
-                perror("accept");   
-                exit(EXIT_FAILURE);   
-            }   
-             
-            //inform user of socket number - used in send and receive commands  
-            printf("New connection , socket fd is %d , ip is : %s , port : %d\n" ,
-            		new_socket , inet_ntoa(address.sin_addr) , ntohs 
-                  (address.sin_port));   
-           
-            //send new connection greeting message  
-            if( send(new_socket, message, strlen(message), 0) != strlen(message) )   
-            {   
-                perror("send");   
-            }   
-                 
-            puts("Welcome message sent successfully");   
-                 
-            //add new socket to array of sockets  
-            for (i = 0; i < max_clients; i++)   
-            {   
-                //if position is empty  
-                if( client_socket[i] == 0 )   
-                {   
-                    client_socket[i] = new_socket;   
-                    printf("Adding to list of sockets as %d\n" , i);   
-                         
-                    break;   
-                }   
-            }   
-        }   
-		else{
+        //If something happened on the master socket, then its an incoming connection so create new socket
+        if (FD_ISSET(mastsoc, &readfds)){   
+
+	        if ((newsoc = accept(mastsoc, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0){   
+	            perror("accept");   
+	            exit(EXIT_FAILURE);   
+	        }   
+	         
+	        //inform user of new socket number    
+	        printf("New connection , socket fd is %d , ip is : %s , port : %d\n" ,
+	        		newsoc , inet_ntoa(address.sin_addr) , ntohs 
+	              (address.sin_port));   
+	       
+	        //send new client a greeting message  
+	        if( send(newsoc, message, strlen(message), 0) != strlen(message) ){   
+	            perror("send");   
+	        }   
+	        puts("Welcome message sent successfully");   
 	             
-	        //else its some IO operation on some other socket 
-	        for (i = 0; i < max_clients; i++)   
-	        {   
-	            sd = client_socket[i];   
+	        //add the new socket to the client socket array  
+	        for (int i = 0; i < maxcls; i++){   
+	            //if position is empty  
+	            if( clsocs[i] == 0 ){   
+	                clsocs[i] = newsoc;   
+	                printf("Adding to list of sockets as %d\n" , i);   
+	                     
+	                break;   
+	            }   
+	        }   
+
+	       
+        }else{
+	 	    //else its some IO operation on some other socket
+	        for (int i = 0; i < maxcls; i++){   
+	            sd = clsocs[i];   
 	                 
-	            if (FD_ISSET( sd , &readfds))   
-	            {   
+	            if (FD_ISSET( sd , &readfds)){   
 	                //Check if it was for closing , and also read the  
 	                //incoming message  
-	                if ((valread = read( sd , buffer, 1024)) == 0)   
-	                {   
+	                if ((valread = read( sd , buffer, 1024)) == 0){   
 	                    //Somebody disconnected , get his details and print  
 	                    getpeername(sd , (struct sockaddr*)&address , 
 	                        (socklen_t*)&addrlen);   
@@ -168,13 +143,11 @@ int main(int argc , char *argv[])
 	                          inet_ntoa(address.sin_addr) , ntohs(address.sin_port));   
 	                         
 	                    //Close the socket and mark as 0 in list for reuse  
-	                    close( sd );   
-	                    client_socket[i] = 0;   
-	                }   
-	                     
+	                    close(sd);   
+	                    clsocs[i] = 0;   
+	                      
 	                //Echo back the message that came in  
-	                else 
-	                {   
+	                }else{   
 	                    //set the string terminating NULL byte on the end  
 	                    //of the data read  
 	                    buffer[valread] = '\0';   
@@ -183,7 +156,9 @@ int main(int argc , char *argv[])
 	            }   
 
 	        }
-        }   
+
+
+    	}
     }   
          
     return 0;   
